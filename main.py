5c418d58e3e869af5ca473b31ed47d7baf1d4cf9
m3957 <86982093+m3957@users.noreply.github.com>
2022-03-16 20:40:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from random import randrange
 
+nb1 = randrange(0,10)
+nb2 = randrange(0,10)
+rep = nb2+nb2
+myrep = int(input(f"{nb1} + {nb2} = "))
+
+if myrep == rep:
+ print("you win")
+else:
+ print("you lose")
